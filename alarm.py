04f1486daba9678147317157103af9a49e8db800
notpqrrot <167501396+notpqrrot@users.noreply.github.com>
2024-05-17 17:10:07 -0700

    Fixed AM/PM glitch, added snooze

--- alarm.py
+++ alarm.py
@@ -1,16 +1,16 @@
 # Simple timer by notpqrrot/lapis
 # Made to account for typing speed and time spent typing out the date so there is no inacurracy in timing
 # Elevator Music by Kevin Mclaeod (not sure how to spell his name...)
-
+# If you dont have playsound installed, simply use os.system command, which is why os has already been imported
 #Inside the parantheses for pygame.mixer.Sound, simply add the path to the sound that you want to play!
-
+import os
 import time
 import datetime
 
 import pygame
 date_format = '%H:%M:%S'
 def countdown(total_seconds):
-    
+
     while total_seconds>0:
         timer = datetime.timedelta(seconds = total_seconds)
         print(timer, end = "\r")
@@ -21,10 +21,10 @@ def countdown(total_seconds):
             print("The timer is at zero!")
             pygame.init()
             pygame.mixer.init()
-            sound = pygame.mixer.Sound('Your sound file path here..')
+            sound = pygame.mixer.Sound('/Users/medhabhutanni/Desktop/Python/sounds/wsg.mp3')
             sound.set_volume(0.75)
             sound.play()
-            
+
             responsed = input("Snooze?: ")
             if responsed == 'no':
 
@@ -34,7 +34,7 @@ def countdown(total_seconds):
                 mins = int(input("How many more minutes?: "))
                 total_seconds = total_seconds + (mins*60)
 
-        
+
 
 current = datetime.datetime.now()
 current1 = current.strftime('%H:%M:%S')
@@ -45,8 +45,11 @@ t1 = time.time()
 typing = t1-t0
 typing = round(typing)
 end = datetime.datetime.strptime(later,'%H:%M:%S')
-if end<now:
+am_pm = input("AM or PM?: ")
+if am_pm == 'AM' and end<now:
     end+=datetime.timedelta(days=1)
+if am_pm == 'PM' and end<now:
+    end+=datetime.timedelta(hours=12)
 
 difference = end - now
 
@@ -55,3 +58,4 @@ total_seconds = total_seconds - typing
 print("Timer has started!")
 print("Time remaining...")
 countdown(total_seconds)
+
